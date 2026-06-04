6bf0086915022d1e855f09334e6d03ab43ce529f
history-study <study@example.com>
2026-06-04 17:06:01 +0800

    feat: 历史选择题每日打卡PWA应用
    - 138道武汉市八下历史选择题，按单元知识点顺序排列
    - 每日打卡10题，顺序推进，错题永久纠错本
    - PWA支持：可安装到手机主屏幕，离线使用
    - 一题一屏手机适配

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'history-quiz-v2';
+const ASSETS = [
+  './打卡练习_历史选择题.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if(resp.ok){
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return resp;
+    }))
+  );
+});
